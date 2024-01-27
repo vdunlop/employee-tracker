1e82284394bffebea59cf5f4d0bd45dce2e45982
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-01-27 10:02:52 -0600

    Got the sql statements working for add department, employee, role, and update an employee's role

--- db/query.sql
+++ db/query.sql
@@ -7,8 +7,35 @@ SELECT * FROM role;
 -- select all from employee for drop down
 SELECT * FROM employee;
 
--- show role title for all employee
+-- show role title and department and emp info for all employee
 SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name
 FROM employee
 LEFT JOIN role ON role.id=employee.role_id
 LEFT JOIN department ON department.id=role.department_id;
+
+-- select role title, department and role info for all roles
+SELECT role.id, role.title, role.salary, department.department_name
+FROM role
+LEFT JOIN department on department.id = role.department_id;
+
+-- add a department
+INSERT INTO
+department(id, department_name)
+VALUES (8, "Groundskeeper");
+
+-- add a role
+INSERT INTO
+role(id, title, salary, department_id)
+VALUES (8, "Data Entry Specialist", 25000, 5);
+
+-- add an employee
+INSERT INTO
+employee(id, first_name, last_name, role_id, manager_id)
+VALUES (8, "April", "Fools", 2, 3);
+
+-- update a role in an employee
+UPDATE employee
+SET
+role_id = 7
+WHERE
+id = 2;
