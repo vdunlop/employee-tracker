b8dcc94846eb0635fe31cd2b99f92db0a0d2e52c
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-01-30 19:15:40 -0600

    function for updating employee role basic logic works. Need to create the helper functions now

--- update-employee-role.js
+++ update-employee-role.js
@@ -1,9 +1,22 @@
 function updateEmployeeRole(db) {
-    const selectAllEmployees = 'SELECT * FROM employee;';
-    db.query(selectAllEmployees, function (err, results) {
-        err ? console.error(err) : console.log(results);
+
+    // Get the role drop down values
+    //const roleId = getRoleDropDown(db);
+    const roleId = 1;
+
+    // Get the employee drop down values
+    // const employeeId = getEmployeesDropDown(db);
+    const employeeId = 1;
+
+    // Build the query
+    const updateRole = `UPDATE employee SET role_id = ${roleId} WHERE id = ${employeeId};`;
+console.log(updateRole);
+
+    db.query(updateRole, function (err, results) {
+        err ? console.error(err) : console.table(results);
     })
     console.log("In updateEmployeeRole");
+    console.log("Updated employee's role");
 };
 
 module.exports = { updateEmployeeRole };
