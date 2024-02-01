e40480e2b9b560116803615e9cc38b79180d7650
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-02-01 15:37:22 -0600

    Update role completed and working

--- update-employee-role.js
+++ update-employee-role.js
@@ -2,24 +2,101 @@
 const inquirer = require("inquirer");
 
 function updateEmployeeRole(db) {
+  let roleList = []; // used for the roles drop down list in the inquirer prompt
+  let employeeList = []; // used for the employees drop down list in the inquirer prompt
 
-    // Get the role drop down values
-    //const roleId = getRoleDropDown(db);
-    const roleId = 1;
+  // Get the role drop down values
+  //const roleId = getRoleDropDown(db);
+  const roleId = 1;
+  const selectAllRoles = "SELECT * FROM role;";
 
-    // Get the employee drop down values
-    // const employeeId = getEmployeesDropDown(db);
-    const employeeId = 1;
+  db.query(selectAllRoles, function (err, roleResults) {
+    if (err) {
+      console.error(err);
+    } else {
+      // parse out the role names
+      for (let i = 0; i < roleResults.length; i++) {
+        roleList[i] = roleResults[i].title;
+      }
+      console.log(roleList);
 
-    // Build the query
-    const updateRole = `UPDATE employee SET role_id = ${roleId} WHERE id = ${employeeId};`;
-console.log(updateRole);
+      // Get the employee drop down values
+      //const employeeId = getemployeeDropDown(db);
+      const employeeId = 2;
+      const selectAllemployees = "SELECT * FROM employee;";
 
-    db.query(updateRole, function (err, results) {
-        err ? console.error(err) : console.table(results);
-    })
-    console.log("In updateEmployeeRole");
-    console.log("Updated employee's role");
-};
+      db.query(selectAllemployees, function (err, employeeResults) {
+        if (err) {
+          console.error(err);
+        } else {
+          // parse out the employee names
+          for (let i = 0; i < employeeResults.length; i++) {
+            employeeList[i] =
+              employeeResults[i].first_name +
+              " " +
+              employeeResults[i].last_name;
+          }
+          console.log(employeeList);
+
+          // Prompt for the employee and role
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                message:
+                  "Which role do you want to assign the selected employee?",
+                name: "roleName",
+                choices: roleList,
+              },
+              {
+                type: "list",
+                message: "Which employee's role do you want to update?",
+                name: "employeeName",
+                choices: employeeList,
+              },
+            ])
+            .then((responses) => {
+              console.log(responses);
+              // get roleId from the query results that matches
+              // the roleName chosen in the inquirer prompt
+              let roleId = 0;
+              for (let i = 0; i < roleResults.length; i++) {
+                if (responses.roleName === roleResults[i].title) {
+                  roleId = roleResults[i].id;
+                  break;
+                }
+              }
+
+              // get employeeId from the query results that matches
+              // the employeeName chosen in the inquirer prompt
+              let employeeId = 0;
+              for (let i = 0; i < employeeResults.length; i++) {
+                if (
+                  responses.employeeName ===
+                  employeeResults[i].first_name +
+                    " " +
+                    employeeResults[i].last_name
+                ) {
+                  employeeId = employeeResults[i].id;
+                  break;
+                }
+              }
+
+              // Build the query
+              const updateEmployeeRole = `UPDATE employee SET role_id = ${roleId} WHERE id = ${employeeId};`;
+              console.log(updateEmployeeRole);
+              db.query(updateEmployeeRole, function (err, finalResults) {
+                err ? console.error(err) : console.table(finalResults);
+              });
+              console.log("In updateEmployee");
+              console.log(
+                `Updated ${responses.employeeName}'s role in the database`
+              );
+            }); // .then inquirer prompt
+        }
+      }); // else for employee
+    }
+  }); // else for role
+} // function end
 
 module.exports = { updateEmployeeRole };
